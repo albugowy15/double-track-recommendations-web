e503508426f67e2c5c4386096f12ea0b6ed59745
albugowy15 <kholidbughowi@gmail.com>
2023-07-05 18:28:27 +0700

    feat: student profile page

---
+++ src/pages/siswa/profil.tsx
@@ -0,0 +1,77 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/Button';
+import { TextInput } from '@/components/Form';
+import Typography from '@/components/Typography';
+
+const studentProfileSchema = z.object({
+  fullname: z.string().nonempty(),
+  password: z.string().nonempty(),
+  nisn: z.string().nonempty(),
+  school: z.string().nonempty(),
+});
+
+type StudentProfileForm = z.infer<typeof studentProfileSchema>;
+
+type StudentProfilePageProps = {
+  fullname: string;
+  nisn: string;
+  password: string;
+  school: string;
+};
+
+export default function StudentProfilePage({
+  fullname,
+  nisn,
+  password,
+  school,
+}: StudentProfilePageProps) {
+  const methods = useForm<StudentProfileForm>({
+    resolver: zodResolver(studentProfileSchema),
+    defaultValues: {
+      fullname,
+      nisn,
+      password,
+      school,
+    },
+  });
+
+  const { handleSubmit } = methods;
+
+  const onSubmit: SubmitHandler<StudentProfileForm> = (_data) => {
+    // TODO: Handle submit
+  };
+  return (
+    <FormProvider {...methods}>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className='mx-auto flex w-full flex-col gap-2 rounded border border-slate-500 p-4 lg:w-[30%]'
+      >
+        <Typography variant='h4' className='text-center'>
+          Biodata Siswa
+        </Typography>
+        <TextInput name='fullname' type='text' label='Nama Lengkap' />
+        <TextInput name='password' type='password' label='Password' />
+        <TextInput name='nisn' type='text' label='NISN' disabled />
+        <TextInput name='school' type='text' label='Sekolah' disabled />
+        <div className='py-2' />
+        <Button variant='filled' className='mx-auto w-fit' type='submit'>
+          Perbarui Biodata
+        </Button>
+      </form>
+    </FormProvider>
+  );
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      fullname: 'Mohamad Kholid Bughowi',
+      nisn: '4254277742',
+      password: 'fhefe663gg4',
+      school: 'SMK Negeri 1 Cimahi',
+    },
+  };
+}
