5ec81ed1226b1c5b478a5aa83722f0814313296d
albugowy15 <kholidbughowi@gmail.com>
2023-11-02 13:30:40 +0700

    feat: navigation item based on user role

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,77 +1,111 @@
 'use client';
 
-import { signIn } from 'next-auth/react';
-import { useState } from 'react';
-import { AiOutlineHome, AiOutlineInfoCircle } from 'react-icons/ai';
-import { BiLogIn } from 'react-icons/bi';
-import { BsBook } from 'react-icons/bs';
-import { CgMenuGridR } from 'react-icons/cg';
+import { LogIn, LogOut, UserCircle } from 'lucide-react';
+import Link from 'next/link';
+import { type Session } from 'next-auth';
+import { signIn, signOut } from 'next-auth/react';
 
-import BasicLink from '@/components/BasicLink';
-import { Button } from '@/components/Button';
-import Modal from '@/components/Modal';
-import Typography from '@/components/typography';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
 
-const Navbar = () => {
-  const [openMenu, setOpenMenu] = useState(false);
+import {
+  adminNavigation,
+  homeNavigation,
+  studentNavigation,
+} from '@/config/navigation';
+
+type NavbarProps = {
+  session: Session | null;
+};
+const Navbar = ({ session }: NavbarProps) => {
   return (
-    <header className='w-full border bg-white shadow-sm'>
+    <header className='w-full border shadow-sm'>
       <div className='container mx-auto flex items-center justify-between px-3 py-2'>
-        <BasicLink href='/' className='text-base font-bold md:text-xl'>
-          DT Rekomendasi
-        </BasicLink>
-        <nav className='flex items-center gap-2'>
-          <Button
-            variant='outlined'
-            icon={CgMenuGridR}
-            onClick={() => setOpenMenu(true)}
-          >
-            Menu
-          </Button>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link href='/' legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  <strong className='text-lg'>DT Rekomendasi</strong>
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            {homeNavigation.map((item, index) => (
+              <NavigationMenuItem key={index}>
+                <Link href={item.href} legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    <span className='font-medium'>{item.title}</span>
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
 
-          <Button variant='filled' icon={BiLogIn} onClick={() => signIn()}>
+        {session ? (
+          <>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant='outline' size='icon'>
+                  <UserCircle className='h-4 w-4' />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>Akunku</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                {session.user.role == 'siswa' ? (
+                  <>
+                    {studentNavigation.map((item, index) => (
+                      <DropdownMenuItem key={index} asChild>
+                        <Link href={item.href}>
+                          {item.icon}
+                          {item.title}
+                        </Link>
+                      </DropdownMenuItem>
+                    ))}
+                  </>
+                ) : (
+                  <>
+                    {adminNavigation.map((item, index) => (
+                      <DropdownMenuItem key={index} asChild>
+                        <Link href={item.href}>
+                          {item.icon}
+                          {item.title}
+                        </Link>
+                      </DropdownMenuItem>
+                    ))}
+                  </>
+                )}
+
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={() => signOut()}>
+                  <LogOut className='mr-2 h-4 w-4' />
+                  <span>Logout</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </>
+        ) : (
+          <Button variant='outline' onClick={() => signIn()}>
+            <LogIn className='mr-2 h-4 w-4' />
             Login
           </Button>
-        </nav>
+        )}
       </div>
-      <Modal isOpen={openMenu} setIsOpen={setOpenMenu} title='Menu Navigation'>
-        <div className='py-2'>
-          <Typography variant='body1' className='text-neutral-400'>
-            Berikut daftar menu yang dapat diakses
-          </Typography>
-          <div className='flex w-full flex-col divide-y divide-neutral-700 py-2 text-neutral-100'>
-            <BasicLink
-              onClick={() => setOpenMenu(false)}
-              href='/'
-              className='flex items-center gap-3 py-3 hover:text-primary-400'
-            >
-              <AiOutlineHome className='text-lg' />
-              Home
-            </BasicLink>
-            <BasicLink
-              onClick={() => setOpenMenu(false)}
-              href='/panduan'
-              className='flex items-center gap-3 py-3 hover:text-primary-400'
-            >
-              <BsBook className='text-lg' />
-              Panduan
-            </BasicLink>
-            <BasicLink
-              onClick={() => setOpenMenu(false)}
-              className='flex items-center gap-3 py-3 hover:text-primary-400'
-              href='/trending'
-            >
-              <AiOutlineInfoCircle className='text-lg' />
-              Tentang
-            </BasicLink>
-          </div>
-          <div className='flex justify-end'>
-            <Button variant='filled' onClick={() => setOpenMenu(false)}>
-              Close
-            </Button>
-          </div>
-        </div>
-      </Modal>
     </header>
   );
 };
