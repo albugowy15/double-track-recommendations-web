f0f40b740a0da9d5fa6ed8ac702dccf9a8b5cc0c
albugowy15 <kholidbughowi@gmail.com>
2023-11-02 13:30:40 +0700

    feat: navigation item based on user role

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,22 +4,27 @@ import '@/styles/globals.css';
 
 import Footer from '@/components/layout/footer';
 import Navbar from '@/components/layout/navbar';
+import { Toaster } from '@/components/ui/toaster';
+
+import { getServerAuthSession } from '@/config/auth';
 
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerAuthSession();
   return (
     <html lang='en' className={inter.className}>
       <head />
       <body className='flex min-h-screen flex-col bg-background font-sans antialiased'>
-        <Navbar />
+        <Toaster />
+        <Navbar session={session} />
         <div className='px-3 pt-5 md:container'>{children}</div>
         <Footer />
       </body>
---
+++ src/config/navigation.tsx
@@ -0,0 +1,70 @@
+import { FileQuestion, LayoutDashboard, Star, User2 } from 'lucide-react';
+
+export type Navigation = {
+  href: string;
+  title: string;
+  icon?: React.ReactNode;
+  description?: string;
+};
+
+export const homeNavigation: Navigation[] = [
+  {
+    href: '/panduan',
+    title: 'Panduan',
+  },
+  {
+    title: 'Tentang',
+    href: '/tentang',
+  },
+];
+
+export const studentNavigation: Navigation[] = [
+  {
+    href: '/siswa/kuesioner',
+    title: 'Kuesioner',
+    icon: <FileQuestion className='mr-2 h-4 w-4' />,
+    description: 'Kuesioner siswa',
+  },
+  {
+    href: '/siswa/profil',
+    title: 'Profil',
+    icon: <User2 className='mr-2 h-4 w-4' />,
+    description: 'Profil siswa',
+  },
+  {
+    href: '/siswa/rekomendasi',
+    title: 'Rekomendasi',
+    icon: <Star className='mr-2 h-4 w-4' />,
+    description: 'Hasil rekomendasi',
+  },
+];
+
+export const adminNavigation: Navigation[] = [
+  {
+    href: '/admin/profil',
+    title: 'Profil',
+    icon: <User2 className='mr-2 h-4 w-4' />,
+    description: 'Profil admin',
+  },
+  {
+    href: '/admin/dashboard',
+    title: 'Dashboard',
+    icon: <LayoutDashboard className='mr-2 h-4 w-4' />,
+    description: 'Dashboard admin',
+  },
+  // {
+  //   href: '/admin/dashboard/hasil-rekomendasi',
+  //   title: 'Hasil Rekomendasi',
+  //   description: 'Dashboard admin hasil rekomendasi',
+  // },
+  // {
+  //   href: '/admin/dashboard/kuesioner',
+  //   title: 'Kuesioner',
+  //   description: 'Dashboard admin kuesioner',
+  // },
+  // {
+  //   href: '/admin/dashboard/siswa',
+  //   title: 'Siswa',
+  //   description: 'Dashboard admin siswa',
+  // },
+];
