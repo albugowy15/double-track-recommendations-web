7f4bbbdb8f9be86297518b6a2ab1a9f1e36c4de0
albugowy15 <kholidbughowi@gmail.com>
2023-11-02 13:58:41 +0700

    refactor: shadcn/ui student profile page

--- src/app/siswa/profil/page.tsx
+++ src/app/siswa/profil/page.tsx
@@ -1,59 +1,32 @@
-'use client';
-
-import { zodResolver } from '@hookform/resolvers/zod';
-import { FormProvider, type SubmitHandler, useForm } from 'react-hook-form';
-import { z } from 'zod';
-
-import { Button } from '@/components/Button';
-import { TextInput } from '@/components/Form';
-import Typography from '@/components/typography';
-
-const studentProfileSchema = z.object({
-  fullname: z.string().nonempty(),
-  password: z.string().nonempty(),
-  nisn: z.string().nonempty(),
-  school: z.string().nonempty(),
-});
-
-type StudentProfileForm = z.infer<typeof studentProfileSchema>;
-
-// type StudentProfilePageProps = {
-//   fullname: string;
-//   nisn: string;
-//   password: string;
-//   school: string;
-// };
+import EditProfileForm from '@/app/siswa/profil/_components/edit-profil-form';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 export default function StudentProfilePage() {
-  const methods = useForm<StudentProfileForm>({
-    resolver: zodResolver(studentProfileSchema),
-  });
-
-  const { handleSubmit } = methods;
-
-  const onSubmit: SubmitHandler<StudentProfileForm> = (_data) => {
-    // TODO: Handle submit
-  };
   return (
-    <>
-      <FormProvider {...methods}>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className='mx-auto flex w-full flex-col gap-2 rounded border border-slate-500 p-4 lg:w-[30%]'
-        >
-          <Typography variant='h4' className='text-center'>
-            Biodata Siswa
-          </Typography>
-          <TextInput name='fullname' type='text' label='Nama Lengkap' />
-          <TextInput name='password' type='password' label='Password' />
-          <TextInput name='nisn' type='text' label='NISN' disabled />
-          <TextInput name='school' type='text' label='Sekolah' disabled />
-          <div className='py-2' />
-          <Button variant='filled' className='mx-auto w-fit' type='submit'>
-            Perbarui Biodata
-          </Button>
-        </form>
-      </FormProvider>
-    </>
+    <Card className='w-fit mx-auto'>
+      <CardHeader>
+        <CardTitle>Profil Siswa</CardTitle>
+        <CardDescription>
+          Berikut adalah profil Anda. Untuk merubah data profil, silahkan ganti
+          data berikut kemudian klik Perbarui Profil.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <EditProfileForm
+          defaultValues={{
+            fullname: 'Nama Siswa nya',
+            nisn: '36463643',
+            password: 'fe766343',
+            school: 'SMA IPIfeym Surabaya',
+          }}
+        />
+      </CardContent>
+    </Card>
   );
 }
