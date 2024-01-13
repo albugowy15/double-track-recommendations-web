6f53cb12774dc53cc31bada0329152c2b792d5a1
storyofhis <azizi.maula@gmail.com>
2024-01-13 18:14:43 +0700

    feat: integrate api entropy and topsis

--- src/app/siswa/rekomendasi/page.tsx
+++ src/app/siswa/rekomendasi/page.tsx
@@ -15,9 +15,56 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
-import { recommendations } from "@/data/rekomendasi";
+import { pembobotan, recommendations } from "@/data/rekomendasi";
+import { protectedFetch } from "@/lib/api";
 
-export default function RecommendationPage() {
+export interface Pembobotan {
+  entrepreneurial_opportunity: Float32Array;
+  facility: Float32Array;
+  interest: Float32Array;
+  salary: Float32Array;
+  total_open_jobs: Float32Array;
+}
+
+export interface getEntropies {
+  fullname: string;
+  school_id: number;
+  student_id: number;
+  total_pembobotan: number;
+  pembobotan: Pembobotan;
+}
+
+export async function getEntropy() {
+  const response = await protectedFetch<getEntropies[]>("/v1/entropy");
+  return response;
+}
+
+export interface Topsis {
+  multimedia: Float32Array;
+  elektro: Float32Array;
+  listrik: Float32Array;
+  boga: Float32Array;
+  busana: Float32Array;
+  kecantikan: Float32Array;
+  mesin: Float32Array;
+}
+export interface getTopsis {
+  student_id: number;
+  school_id: number;
+  fullname: string;
+  TOPSIS_Score: Topsis;
+}
+
+export async function getEntropyTOPSIS() {
+  return await protectedFetch<getTopsis[]>("/v1/entropy-topsis");
+}
+
+export default async function RecommendationPage() {
+  const entropy = await getEntropy();
+
+  const sortDataByScore = (data: any[]) => {
+    return data.slice().sort((a, b) => b.score - a.score);
+  };
   return (
     <main className="px-3 pt-5 md:container">
       <Typography variant="h2">
@@ -25,8 +72,41 @@ export default function RecommendationPage() {
       </Typography>
       <Typography variant="body1">
         Berikut hasil rekomendasi bidang keterampilan yang sesuai dengan
-        preferensimu.
+        preferensi ananda :
+        <Typography variant="body1" fontWeight="bold">
+          {entropy.data.fullname}
+        </Typography>
       </Typography>
+      <Card>
+        <CardHeader>
+          <CardTitle>Pembobotan Kriteria</CardTitle>
+          <CardDescription>
+            Pembobotan Kriteria Menggunakan Perhitungan Metode Entropy
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="relative overflow-x-auto text-sm shadow-md sm:rounded-lg">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Pembobotan</TableHead>
+                  <TableHead>Nilai</TableHead>
+                  <TableHead>Percantage</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {pembobotan.data.map((weight, index) => (
+                  <TableRow key={index}>
+                    <TableHead>{weight.weight}</TableHead>
+                    <TableHead>{weight.value}</TableHead>
+                    <TableHead>{weight.percentage} %</TableHead>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </CardContent>
+      </Card>
       <div className="mt-4 flex flex-col items-center gap-2 lg:flex-row">
         {recommendations.map((recommendation, index) => (
           <Card key={index}>
@@ -46,9 +126,9 @@ export default function RecommendationPage() {
                     </TableRow>
                   </TableHeader>
                   <TableBody>
-                    {recommendation.data.map((data, idx) => (
+                    {sortDataByScore(recommendation.data).map((data, idx) => (
                       <TableRow key={idx}>
-                        <TableCell>{data.ranking}</TableCell>
+                        <TableCell>{idx + 1}</TableCell>
                         <TableCell>{data.skill}</TableCell>
                         <TableCell>{data.score}</TableCell>
                         <TableCell>{data.details}</TableCell>
--- src/data/rekomendasi.ts
+++ src/data/rekomendasi.ts
@@ -1,4 +1,9 @@
 import { type RecommendationResult } from "@/app/admin/dashboard/hasil-rekomendasi/_column";
+import {
+  getEntropyTOPSIS,
+  type getEntropies,
+} from "@/app/siswa/rekomendasi/page";
+import { protectedFetch } from "@/lib/api";
 
 interface Rekomendasi {
   method: string;
@@ -11,23 +16,117 @@ interface Rekomendasi {
   }[];
 }
 
-export const recommendations: Rekomendasi[] = [
+interface PembobotanData {
+  data: {
+    weight: string;
+    value: Float32Array;
+    percentage: number;
+  }[];
+}
+
+async function getEntropy() {
+  const response = await protectedFetch<getEntropies[]>("/v1/entropy");
+  return response;
+}
+function decimalToPercentage(
+  decimalValue: number | undefined,
+  decimalPlaces = 2,
+): number {
+  if (decimalValue === undefined || isNaN(decimalValue)) {
+    return 0; // or return a default value as needed
+  }
+
+  const percentageValue = decimalValue * 100;
+  const roundedPercentage =
+    Math.round(percentageValue * 10 ** decimalPlaces) / 10 ** decimalPlaces;
+  return roundedPercentage;
+}
+
+const entropy = await getEntropy();
+export const pembobotan: PembobotanData = {
+  data: [
+    {
+      weight: "Peluang Wirausaha",
+      value: entropy.data.pembobotan.entrepreneurial_opportunity,
+      percentage: decimalToPercentage(
+        entropy.data.pembobotan.entrepreneurial_opportunity,
+      ),
+    },
+    {
+      weight: "Fasilitas Pendukung",
+      value: entropy.data.pembobotan.facility,
+      percentage: decimalToPercentage(entropy.data.pembobotan.facility),
+    },
+    {
+      weight: "Ketertarikan Siswa Terhadap sebuah Bidang",
+      value: entropy.data.pembobotan.interest,
+      percentage: decimalToPercentage(entropy.data.pembobotan.interest),
+    },
+    {
+      weight: "Gaji",
+      value: entropy.data.pembobotan.salary,
+      percentage: decimalToPercentage(entropy.data.pembobotan.salary),
+    },
+    {
+      weight: "Lapangan Usaha",
+      value: entropy.data.pembobotan.total_open_jobs,
+      percentage: decimalToPercentage(entropy.data.pembobotan.total_open_jobs),
+    },
+  ],
+};
+
+const entropy_topsis = await getEntropyTOPSIS();
+interface TopsisData {
+  method: string;
+  description: string;
+  data: {
+    skill: string;
+    score: Float32Array;
+    percentage: number;
+  }[];
+}
+export const recommendations: TopsisData[] = [
   {
     method: "AHP",
     description:
       "Berikut hasil rekomendasi keterampilan yang sesuai untuk Anda dengan metode AHP",
     data: [
       {
-        ranking: 1,
         skill: "Tata Busana",
-        score: 0.5,
+        score: entropy_topsis.data.TOPSIS_Score.busana,
         details: "Busana, fashion, dan tata rias",
       },
       {
-        ranking: 2,
         skill: "Multimedia",
-        score: 0.3,
-        details: "Desaing grafis, animasi, dan video editing",
+        score: entropy_topsis.data.TOPSIS_Score.multimedia,
+        details: "Desain grafis, animasi, dan video editing",
+      },
+      {
+        skill: "Teknik Elektro",
+        score: entropy_topsis.data.TOPSIS_Score.elektro,
+        details: "Teknik Elektro, elektronika, dan pengolahan sinyal",
+      },
+      {
+        skill: "Teknik Listrik",
+        score: entropy_topsis.data.TOPSIS_Score.listrik,
+        details:
+          "Teknik Listrik, instalasi listrik, dan perawatan peralatan listrik",
+      },
+      {
+        skill: "Tata Boga",
+        score: entropy_topsis.data.TOPSIS_Score.boga,
+        details: "Tata Boga, kuliner, dan seni memasak",
+      },
+      {
+        skill: "Tata Kecantikan",
+        score: entropy_topsis.data.TOPSIS_Score.kecantikan,
+        details: "Tata Kecantikan, perawatan kulit, Salon, dan kecantikan",
+      },
+      {
+        skill: "Teknik Kendaraan Ringan/Motor",
+        score: entropy_topsis.data.TOPSIS_Score.mesin,
+        details:
+          "Teknik Mesin, Teknik Kendaraan Ringan/Motor, perbaikan mesin kendaraan bermotor",
       },
     ],
   },
@@ -37,16 +136,41 @@ export const recommendations: Rekomendasi[] = [
       "Berikut hasil rekomendasi keterampilan yang sesuai untuk Anda dengan metode TOPSIS",
     data: [
       {
-        ranking: 1,
         skill: "Tata Busana",
-        score: 0.5,
+        score: entropy_topsis.data.TOPSIS_Score.busana,
         details: "Busana, fashion, dan tata rias",
       },
       {
-        ranking: 2,
         skill: "Multimedia",
-        score: 0.3,
-        details: "Desaing grafis, animasi, dan video editing",
+        score: entropy_topsis.data.TOPSIS_Score.multimedia,
+        details: "Desain grafis, animasi, dan video editing",
+      },
+      {
+        skill: "Teknik Elektro",
+        score: entropy_topsis.data.TOPSIS_Score.elektro,
+        details: "Teknik Elektro, elektronika, dan pengolahan sinyal",
+      },
+      {
+        skill: "Teknik Listrik",
+        score: entropy_topsis.data.TOPSIS_Score.listrik,
+        details:
+          "Teknik Listrik, instalasi listrik, dan perawatan peralatan listrik",
+      },
+      {
+        skill: "Tata Boga",
+        score: entropy_topsis.data.TOPSIS_Score.boga,
+        details: "Tata Boga, kuliner, dan seni memasak",
+      },
+      {
+        skill: "Tata Kecantikan",
+        score: entropy_topsis.data.TOPSIS_Score.kecantikan,
+        details: "Tata Kecantikan, perawatan kulit, Salon, dan kecantikan",
+      },
+      {
+        skill: "Teknik Kendaraan Ringan/Motor",
+        score: entropy_topsis.data.TOPSIS_Score.mesin,
+        details:
+          "Teknik Mesin, Teknik Kendaraan Ringan/Motor, perbaikan mesin kendaraan bermotor",
       },
     ],
   },
