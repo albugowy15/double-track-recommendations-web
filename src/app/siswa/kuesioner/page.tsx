0da2f2dfb7509d35ad51f5b768b2715b52ee538d
albugowy15 <kholidbughowi@gmail.com>
2024-03-16 14:28:48 +0700

    feat: show list questions

--- src/app/siswa/kuesioner/page.tsx
+++ src/app/siswa/kuesioner/page.tsx
@@ -1,75 +1,28 @@
-"use client";
-
-import clsx from "clsx";
-import { useForm } from "react-hook-form";
-
+import { type Question } from "@/data/kuesioner";
+import { QuestionnareForm } from "./_components/questionnare_form";
+import { protectedFetch } from "@/lib/api";
+import { type Metadata } from "next";
 import Typography from "@/components/typography";
 
-import { questionnare } from "@/data/kuesioner";
-import { Button } from "@/components/ui/button";
-
-export default function QuestionnarePage() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  const onSubmit = () => {
-    //
-  };
+export const metadata: Metadata = {
+  title: "Kuesioner siswa",
+};
+export default async function QuestionnarePage() {
+  const response = await protectedFetch<Question[]>(
+    "/v1/questionnare/questions",
+  );
+  const questions = response?.data ? response.data : [];
   return (
     <main className="px-3 pt-5 md:container">
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="mx-auto flex flex-col gap-3 lg:max-w-4xl"
-      >
-        <Typography variant="h3" className="text-center">
-          Kuesioner Preferensi Siswa Pada Keterampilan Double Track
-        </Typography>
-        <Typography variant="body1" className="text-center">
-          Diharapkan mengisi kuesioner ini dengan sungguh-sungguh dan teliti
-          agar mendapatkan rekomendasi keterampilan yang dapat Anda ambil dengan
-          akurat.
-        </Typography>
-        {questionnare.map((question, index) => (
-          <div key={question.id} className="space-y-2 rounded border p-3">
-            <Typography variant="body1" className="font-bold">
-              {index + 1}. {question.question}{" "}
-              <span className="text-red-500">*</span>
-            </Typography>
-            <div
-              className={clsx("flex justify-start gap-3", [
-                question.options.length > 4
-                  ? "flex-col items-start md:flex-row md:items-end"
-                  : "flex-row items-end",
-              ])}
-            >
-              <Typography variant="body1">{question.minText}</Typography>
-              {question.options?.map((option, index) => (
-                <div key={index} className="flex flex-col items-center gap-1">
-                  <label className="text-sm">{option}</label>
-                  <input
-                    type="radio"
-                    value={option}
-                    {...register(question.id, { required: true })}
-                  />
-                </div>
-              ))}
-
-              <Typography variant="body1">{question.maxText}</Typography>
-            </div>
-            {errors[question.id] ? (
-              <Typography variant="label1" className="text-red-600">
-                Pertanyaan ini wajib diisi
-              </Typography>
-            ) : null}
-          </div>
-        ))}
-        <Button type="submit" className="w-fit">
-          Submit
-        </Button>
-      </form>
+      <Typography variant="h3" className="text-center">
+        Kuesioner Preferensi Siswa Pada Keterampilan Double Track
+      </Typography>
+      <Typography variant="body1" className="text-center max-w-xl mx-auto mb-7">
+        Diharapkan mengisi kuesioner ini dengan sungguh-sungguh dan teliti agar
+        mendapatkan rekomendasi keterampilan yang dapat Anda ambil dengan
+        akurat.
+      </Typography>
+      <QuestionnareForm questions={questions} />
     </main>
   );
 }
--- src/data/kuesioner.ts
+++ src/data/kuesioner.ts
@@ -1,62 +1,145 @@
-interface Question {
-  id: string;
+export interface Question {
+  id: number;
+  number: number;
   question: string;
   options: string[];
   type: "text" | "radio" | "range";
-  minText?: string;
-  maxText?: string;
+  min_text?: string;
+  max_text?: string;
 }
 
 export const questionnare: Question[] = [
   {
-    id: "434343",
-    question: "Seberapa besar minatmu terhadap multimedia",
+    id: 1,
+    number: 1,
+    question: "Seberapa besar minatmu terhadap multimedia?",
     options: ["1", "2", "3", "4"],
     type: "radio",
-    minText: "Tidak tertarik",
-    maxText: "Sangat tertarik",
+    min_text: "Tidak Berminat",
+    max_text: "Sangat Berminat",
   },
   {
-    id: "32iid77e",
+    id: 2,
+    number: 2,
     question: "Seberapa besar minatmu terhadap teknik elektro?",
     options: ["1", "2", "3", "4"],
     type: "radio",
-    minText: "Tidak tertarik",
-    maxText: "Sangat tertarik",
+    min_text: "Tidak Berminat",
+    max_text: "Sangat Berminat",
   },
   {
-    id: "73hcjs2",
+    id: 3,
+    number: 3,
     question: "Seberapa besar minatmu terhadap teknik listrik?",
     options: ["1", "2", "3", "4"],
     type: "radio",
-    minText: "Tidak tertarik",
-    maxText: "Sangat tertarik",
+    min_text: "Tidak Berminat",
+    max_text: "Sangat Berminat",
   },
   {
-    id: "dj3kie8e",
-    question:
-      "Seberapa dukungan fasilitas yang kamu miliki untuk bisa mengikuti keterampilan tata busana?",
+    id: 4,
+    number: 4,
+    question: "Seberapa besar minatmu terhadap tata busana?",
     options: ["1", "2", "3", "4"],
     type: "radio",
-    minText: "Tidak mendukung",
-    maxText: "Sangat mendukung",
+    min_text: "Tidak Berminat",
+    max_text: "Sangat Berminat",
   },
   {
-    id: "1234fghy",
-    question:
-      "Dalam memilih bidang keterampilan, seberapa pentingnya jumlah lapangan pekerjaan dengan gaji?",
-    options: ["9", "7", "5", "3", "1", "1/3", "1/5", "1/7", "1/9"],
-    type: "range",
-    minText: "Lapangan pekerjaan lebih penting",
-    maxText: "Gaji lebih penting",
+    id: 5,
+    number: 5,
+    question: "Seberapa besar minatmu terhadap tata boga?",
+    options: ["1", "2", "3", "4"],
+    type: "radio",
+    min_text: "Tidak Berminat",
+    max_text: "Sangat Berminat",
   },
   {
-    id: "084j8fjs",
+    id: 6,
+    number: 6,
+    question: "Seberapa besar minatmu terhadap tata kecantikan?",
+    options: ["1", "2", "3", "4"],
+    type: "radio",
+    min_text: "Tidak Berminat",
+    max_text: "Sangat Berminat",
+  },
+  {
+    id: 7,
+    number: 7,
+    question: "Seberapa besar minatmu terhadap teknik kendaraan ringan/motor?",
+    options: ["1", "2", "3", "4"],
+    type: "radio",
+    min_text: "Tidak Berminat",
+    max_text: "Sangat Berminat",
+  },
+  {
+    id: 8,
+    number: 8,
     question:
-      "Dalam memilih bidang keterampilan, seberapa pentingnya jumlah lapangan pekerjaan dengan peluang wirausaha?",
-    options: ["9", "7", "5", "3", "1", "1/3", "1/5", "1/7", "1/9"],
-    type: "range",
-    minText: "Lapangan pekerjaan lebih penting",
-    maxText: "Peluang wirausaha lebih penting",
+      "Seberapa dukungan fasilitas yang kamu miliki untuk bisa mengikuti keterampilan Multimedia?",
+    options: ["1", "2", "3", "4"],
+    type: "radio",
+    min_text: "Tidak Mendukung",
+    max_text: "Sangat Mendukung",
+  },
+  {
+    id: 9,
+    number: 9,
+    question:
+      "Seberapa dukungan fasilitas yang kamu miliki untuk bisa mengikuti keterampilan Teknik Elektro?",
+    options: ["1", "2", "3", "4"],
+    type: "radio",
+    min_text: "Tidak Mendukung",
+    max_text: "Sangat Mendukung",
+  },
+  {
+    id: 10,
+    number: 10,
+    question:
+      "Seberapa dukungan fasilitas yang kamu miliki untuk bisa mengikuti keterampilan Teknik Lisrik?",
+    options: ["1", "2", "3", "4"],
+    type: "radio",
+    min_text: "Tidak Mendukung",
+    max_text: "Sangat Mendukung",
+  },
+  {
+    id: 11,
+    number: 11,
+    question:
+      "Seberapa dukungan fasilitas yang kamu miliki untuk bisa mengikuti keterampilan Tata Busana?",
+    options: ["1", "2", "3", "4"],
+    type: "radio",
+    min_text: "Tidak Mendukung",
+    max_text: "Sangat Mendukung",
+  },
+  {
+    id: 12,
+    number: 12,
+    question:
+      "Seberapa dukungan fasilitas yang kamu miliki untuk bisa mengikuti keterampilan Tata Boga?",
+    options: ["1", "2", "3", "4"],
+    type: "radio",
+    min_text: "Tidak Mendukung",
+    max_text: "Sangat Mendukung",
+  },
+  {
+    id: 13,
+    number: 13,
+    question:
+      "Seberapa dukungan fasilitas yang kamu miliki untuk bisa mengikuti keterampilan Tata Kecantikan",
+    options: ["1", "2", "3", "4"],
+    type: "radio",
+    min_text: "Tidak Mendukung",
+    max_text: "Sangat Mendukung",
+  },
+  {
+    id: 14,
+    number: 14,
+    question:
+      "Seberapa dukungan fasilitas yang kamu miliki untuk bisa mengikuti keterampilan Teknik Kendararaan Ringan/Motor?",
+    options: ["1", "2", "3", "4"],
+    type: "radio",
+    min_text: "Tidak Mendukung",
+    max_text: "Sangat Mendukung",
   },
 ];
