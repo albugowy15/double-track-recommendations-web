62935c5c7e9b333315a2bfada830c9c5d68a9ce3
albugowy15 <kholidbughowi@gmail.com>
2024-03-16 14:27:48 +0700

    feat: support custom method and body

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,15 +5,19 @@ import { redirect } from "next/navigation";
 
 /**
  * Fetches data from a protected endpoint.
- * Make sure to only call this function inside Server Component
+ * Make sure to only call this function inside Server Component.
  *
  * @template T The expected return type from the API endpoint.
  * @param {string} endpoint - The API endpoint to fetch data from.
+ * @param {"GET" | "POST" | "PATCH" | "DELETE"} [method="GET"] - The HTTP method for the request.
+ * @param {*} [body] - The request body.
  * @returns {Promise<APIResponse<T>>} The data returned from the API endpoint.
- * @throws Will throw an error if the fetch request fails.
+ * @throws {Error} Will throw an error if the fetch request fails.
  */
 export async function protectedFetch<T>(
   endpoint: string,
+  method?: "GET" | "POST" | "PATCH" | "DELETE",
+  body?: unknown,
 ): Promise<APIResponse<T>> {
   const url = env.NEXT_PUBLIC_API_URL + endpoint;
 
@@ -21,33 +25,39 @@ export async function protectedFetch<T>(
   if (!session) redirect("403");
 
   const res = await fetch(url, {
-    method: "GET",
+    method: method ?? "GET",
     headers: {
       "Content-Type": "application/json",
       Authorization: "Bearer " + session.user.token,
     },
+    body: body ? JSON.stringify(body) : undefined,
   });
   return (await res.json()) as APIResponse<T>;
 }
 
 /**
  * Fetches data from a public endpoint.
- * Make sure to only call this function inside Server Component
+ * Make sure to only call this function inside Server Component.
  *
  * @template T The expected return type from the API endpoint.
  * @param {string} endpoint - The API endpoint to fetch data from.
+ * @param {"GET" | "POST" | "PATCH" | "DELETE"} [method="GET"] - The HTTP method for the request.
+ * @param {*} [body] - The request body.
  * @returns {Promise<APIResponse<T>>} The data returned from the API endpoint.
- * @throws Will throw an error if the fetch request fails.
+ * @throws {Error} Will throw an error if the fetch request fails.
  */
 export async function publicFetch<T>(
   endpoint: string,
+  method?: "GET" | "POST" | "PATCH" | "DELETE",
+  body?: unknown,
 ): Promise<APIResponse<T>> {
   const url = env.NEXT_PUBLIC_API_URL + endpoint;
   const res = await fetch(url, {
-    method: "GET",
+    method: method ?? "GET",
     headers: {
       "Content-Type": "application/json",
     },
+    body: body ? JSON.stringify(body) : undefined,
   });
   return (await res.json()) as APIResponse<T>;
 }
