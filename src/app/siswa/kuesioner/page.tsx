2be1099137500eead36164535f9015acc27243d8
albugowy15 <kholidbughowi@gmail.com>
2024-03-24 21:42:34 +0700

    feat: check is questionnare ready before procedd

--- src/app/siswa/kuesioner/page.tsx
+++ src/app/siswa/kuesioner/page.tsx
@@ -3,11 +3,32 @@ import { QuestionnareForm } from "./_components/questionnare_form";
 import { protectedFetch } from "@/lib/api";
 import { type Metadata } from "next";
 import Typography from "@/components/typography";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { TriangleAlert } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "Kuesioner siswa",
 };
 export default async function QuestionnarePage() {
+  const questionnareReadyRes = await protectedFetch<{ ready: boolean }>(
+    "/v1/questionnare/ready",
+  );
+  if (!questionnareReadyRes?.data) return null;
+
+  if (!questionnareReadyRes?.data.ready) {
+    return (
+      <main className="pt-20 flex justify-center">
+        <Alert variant="destructive" className="max-w-xl">
+          <TriangleAlert className="h-4 w-4" />
+          <AlertTitle>Perhatian!</AlertTitle>
+          <AlertDescription>
+            Admin belum mengatur kuesioner sehingga Anda belum diperkenankan
+            untuk mengisi kuesioner. Silahkan hubungi admin.
+          </AlertDescription>
+        </Alert>
+      </main>
+    );
+  }
   const response = await protectedFetch<Question[]>(
     "/v1/questionnare/questions",
   );
